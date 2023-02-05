caebd3a15be953411f9b0d0d24e3738debbaa490
matheus mendes <matheusmendesreis@outlook.com>
2023-02-05 12:21:57 -0300

    estado global criado

---
+++ loja-virtual/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { GlobalState } from './globalStates/GlobalState';
+
+function App() {
+  return (
+    <GlobalState >
+      
+    </GlobalState>
+  );
+}
+
+export default App;
