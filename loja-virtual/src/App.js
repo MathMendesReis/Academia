a51249db5903720087a93b21faf0ac9d492a13b2
matheus mendes <matheusmendesreis@outlook.com>
2023-02-17 22:07:32 -0300

    style:estado global criaado

--- loja-virtual/src/App.js
+++ loja-virtual/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { GlobalState } from './globalStates/GlobalState';
+import { GlobalStyled } from './globalStyled/globalStyled';
 
 function App() {
   return (
     <GlobalState >
-      
+      <GlobalStyled />
+
     </GlobalState>
   );
 }
---
+++ loja-virtual/src/globalStyled/globalStyled.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyled = createGlobalStyle`
+*{
+    background-color: #1E1E1E;
+    font-family: Montserrat;
+    color: #FFFFFF;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+img{
+    height: 623px;
+    width: 484px
+;
+}
+
+button{
+    cursor: pointer;
+    border: none;
+}
+`
