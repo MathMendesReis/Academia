5d71399763c5d65762982c9ae30119853d58487d
matheus mendes <matheusmendesreis@outlook.com>
2023-02-18 18:40:12 -0300

    inicializando sessão do treinador

---
+++ loja-virtual/src/components/SectionTreinador/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BoxImgs, BoxTexts } from "../header/styled";
+import { BoxContainer } from "./styled";
+
+export const CardTreinador = () => {
+  return (
+    <BoxContainer>
+      <BoxTexts>
+        <h1>quem é seu treinador</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. In augue egestas ullamcorper
+          nec viverra orci risus egestas. Lacus consequat auctor lacus amet
+          volutpat sed. Pharetra suscipit in vitae ante etiam in. Convallis
+          blandit dolor in nisl malesuada venenatis dignissim mauris. Nisl mi
+          vehicula cursus nisi sit nulla donec adipiscing. Ipsum nibh aliquam
+          integer faucibus purus. Orci eu malesuada morbi et interdum nunc lorem
+          a adipiscing. Nisl suscipit maecenas risus amet at convallis
+          massa.Lorem ipsum dolor sit amet consectetur. In augue egestas
+          ullamcorper nec viverra orci risus egestas. Lacus consequat auctor
+          lacus amet volutpat sed. Pharetra suscipit in vitae ante etiam in.
+          Convallis blandit dolor in nisl malesuada venenatis dignissim mauris.
+        </p>
+        <div></div>
+      </BoxTexts>
+      <BoxImgs>box2</BoxImgs>
+    </BoxContainer>
+  );
+};
---
+++ loja-virtual/src/components/cardsComentarios/styled.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const BoxNameANdProf = styled.div`
+display: flex;
+gap: 10px;
+align-items: center;
+background-color: transparent;
+
+`
+
+export const Name = styled.h1`
+display: block;
+width: 100%;
+font-size: 2.5vw;
+background-color: #1E1E1E;
+
+`
+
+export const Profissao = styled.h3`
+display: block;
+font-size: 1.9vw;
+background-color: #1E1E1E;
+color: #E2181A;
+
+`
+export const BoxComents = styled.div`
+border-bottom: 2px solid white;
+height: 200px;
+width: 180px;
+padding: 5px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+background-color: #1E1E1E;
+
+`
+
+export const ImgLogo = styled.img`
+padding:5px;
+margin-left: 120px;
+`
