ae17a0555aedcda2cd6b684a1dd387914348257a
MathMendesReis <matheusmendesreis@outlook.com>
2023-06-30 16:39:49 -0300

    limpando projeto

--- loja-virtual/src/components/cardSectionOne/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const CardBox = styled.section`
-display: flex;
-justify-content: center;
-align-items: center;
-gap: 15%;
-flex-direction: column;
-`
--- loja-virtual/src/globalStyled/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Body = styled.div`
-display: flex;
-flex-direction: column;
-height: auto;
-width: 100vw;
-`
